97f2ac05c29a248251c4af79a0d50bd8b4dd1061
EllitonDias <92165656+EllitonDias@users.noreply.github.com>
2022-04-30 20:24:05 -0300

    Add files via upload

--- FirstApp/app/src/main/java/br/com/ufc/firstapp/MainActivity2.java
+++ FirstApp/app/src/main/java/br/com/ufc/firstapp/MainActivity2.java
@@ -1,19 +1,22 @@
 package br.com.ufc.firstapp;
 
-import static br.com.ufc.firstapp.R.layout.*;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 
 public class MainActivity2 extends AppCompatActivity {
-
+    MediaPlayer Morena;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        setTitle("Agendamento");
+        Morena = MediaPlayer.create(this,R.raw.Morena);
+    }
 
-
+    public void PlayMusica(View view) {
+        Morena.start();
     }
 }
