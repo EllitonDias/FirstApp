f4353a640d667230a0b9d81261ff6797b6ea3491
EllitonDias <92165656+EllitonDias@users.noreply.github.com>
2022-04-30 19:04:45 -0300

    Add files via upload

---
+++ FirstApp/app/src/main/java/br/com/ufc/firstapp/MainActivity.java
@@ -0,0 +1,78 @@
+package br.com.ufc.firstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText  nome, telefone, email;
+    private RadioButton masculino, feminino;
+    private TextView resNome, resTelefone, resEmail,resSexo;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nome = findViewById(R.id.activity_main_nome);
+        telefone = findViewById(R.id.activity_main_telefone);
+        email = findViewById(R.id.activity_main_email);
+
+        masculino = findViewById(R.id.activity_main_masculino);
+        feminino = findViewById(R.id.activity_main_feminino);
+
+        resNome = findViewById(R.id.activity_main_resNome);
+        resTelefone = findViewById(R.id.activity_main_resTelefone);
+        resEmail = findViewById(R.id.activity_main_resEmail);
+        resSexo = findViewById(R.id.activity_main_sexo);
+
+        Button btnCadastrar = findViewById(R.id.activity_main_btn_cadastrar);
+
+        btnCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String editNome = nome.getText().toString();
+                String editTelefone = telefone.getText().toString();
+                String editEmail = email.getText().toString();
+
+                if (editNome.isEmpty() || editTelefone.isEmpty() || editEmail.isEmpty()) {
+                    Snackbar.make(v, "Preencha todos os campos", Snackbar.LENGTH_LONG).show();
+                } else {
+                    btnRadio(v);
+                }
+            }
+        });
+    }
+    private void btnRadio(View view){
+
+        String editNome = nome.getText().toString();
+        String editTelefone = telefone.getText().toString();
+        String editEmail = email.getText().toString();
+
+        if(masculino.isChecked()){
+            resNome.setText(editNome);
+            resTelefone.setText(editTelefone);
+            resEmail.setText(editEmail);
+            resSexo.setText("Masculino");
+        }else if(feminino.isChecked()){
+            resNome.setText(editNome);
+            resTelefone.setText(editTelefone);
+            resEmail.setText(editEmail);
+            resSexo.setText("Feminino");
+        }else{
+            Snackbar.make(view, "Preencha o sexo", Snackbar.LENGTH_SHORT).show();
+        }
+    }
+
+    public void chamarActivity2(View view) {
+        Intent activity2 = new Intent(this,MainActivity2.class);
+        startActivity(activity2);
+    }
+}
---
+++ FirstApp/app/src/main/java/br/com/ufc/firstapp/MainActivity2.java
@@ -0,0 +1,19 @@
+package br.com.ufc.firstapp;
+
+import static br.com.ufc.firstapp.R.layout.*;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        setTitle("Agendamento");
+
+
+    }
+}
